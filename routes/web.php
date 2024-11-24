80a7fbfdb0360432581ffb7dbd10bfb53a0ccc1b
Claudio Filho <claudiofilho1525@gmail.com>
2024-11-24 18:31:01 -0300

    new: Create middlware for multi auth and redirect to dashboard depending on role

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
+    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'role:admin'], function() {
         Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
     });
 });

60c8f5f6cab4671ebc30f4678b99242fc50a1de7
Claudio Filho <claudiofilho1525@gmail.com>
2024-11-25 13:09:33 -0300

    new: Create separate route files for admin

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'role:admin'], function() {
-        Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-    });
 });
 
 require __DIR__.'/auth.php';
